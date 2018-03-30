0caaad28538419edf9b0c07e6f6cfeafec918da2
Madeline Stark <madeline.stark1@gmail.com>
2018-03-30 22:38:26 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -21,7 +21,7 @@ programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
-	
+
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -56,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+		 programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
